28228373bfe768eb7b68af1097a953d824e99973
rm1269 <rm1269@gmail.com>
2011-03-08 10:43:35 +0000

    Update german texts

--- Android/src/com/anoshenko/android/mahjongg/PlayActivity.java
+++ Android/src/com/anoshenko/android/mahjongg/PlayActivity.java
@@ -20,7 +20,6 @@ import android.widget.TextView;
 
 import com.anoshenko.android.toolbar.ToolbarButton;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Random;
